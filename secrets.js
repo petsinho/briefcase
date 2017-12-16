542bc6558d3621a4c9179fc288aeda06ffacea83
petsinho <panagiotis.petsas@gmail.com>
2017-12-16 12:26:39 +0100

    added dummy secrets.js file and warning for not configured AWS settings

---
+++ secrets.js
@@ -0,0 +1,10 @@
+const AwsOptions = {
+  keyPrefix: 'uploads/briefcase/',
+  bucket: 'your-bucket-name',
+  region: 'your-region',
+  accessKey: 'aws-access-key',
+  secretKey: 'aws-secret-key',
+  successActionStatus: 201,
+};
+
+export default AwsOptions;
